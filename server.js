816451b4dbcdf26f21085f37a5e5c1717208406b
Bui Huy Hung <huyhung.dev@gmail.com>
2018-10-02 19:01:49 +0700

    initializing project

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+
