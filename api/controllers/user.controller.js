9d06db4dad9b84de60db46ddf20ba0c2ff5a845d
Bui Huy Hung <huyhung.dev@gmail.com>
2018-10-04 19:44:21 +0700

    update async await

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -3,49 +3,60 @@ const User = require('../models/user.model.js');
 
 // @controller   GET api/users
 // @desc         Get all users
-module.exports.getAllUser = (req, res) => {
-	User.find()
-		.then(users => res.json(users));
+module.exports.getAllUser = async (req, res) => {
+	const users = await User.find();
+	res.json(users);
 };
 
 // @controller   GET api/users
 // @desc         Get user by id, if can't find, default return false
-module.exports.getUser = (req, res) => {
-	User.findById(req.params.id)
-		.then(user => res.json(user));
+module.exports.getUser = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		res.json(user);
+	} catch(err) {
+		res.json(err);
+	}
 };
 
 // @controller   POST api/users
 // @desc         Create new user
-module.exports.createUser = (req, res) => {
+module.exports.createUser = async (req, res) => {
 	const newUser = new User({
 		email: req.body.email,
 		password: req.body.password,
 		name: req.body.name,
 		phone: req.body.phone
 	});
-
-	newUser.save()
-		.then(user => res.json({success: true, user: user}))
-		.catch(err => res.status(404).json({success: false, error: err.message}));
+	try {
+		const user = await newUser.save();
+		res.json({success: true, user: user});
+	} catch(err) {
+		res.status(404).json({success: false, error: err.message});
+	}
 };
 
 // @controller   PATCH api/users/:id
-// @desc         Update user info, import into an Object
-module.exports.updateUser = (req, res) => {
+// @desc         Update user info, import into an Object with key are: update
+module.exports.updateUser = async (req, res) => {
 	const id = req.params.id;
-	User.findOneAndUpdate(
-		{'_id': id }, { $set: req.body.update }, { new: true }, (error, doc) => {
-			if (error) return res.status(404).json({success: false, error: error.message});
-			res.json({success: true, user: doc});
-		}
-	)
+	try {
+		const user = await User.findOneAndUpdate(
+			{'_id': id }, { $set: req.body.update }, { new: true }
+		)
+		res.json({user: user});
+	} catch(err) {
+		res.status(404).json({success: false, error: error.message});
+	}
 };
 
 // @controller   DELETE api/users/:id
 // @desc         Delete a user
-module.exports.deleteUser = (req, res) => {
-	User.findById(req.params.id)
-		.then(user => user.remove().then(() => res.json({success: true})))
-		.catch(err => res.status(404).json({success: false, error: err.message}));
+module.exports.deleteUser = async (req, res) => {
+	try {
+		const user = await User.findById(req.params.id);
+		user.remove().then(() => res.json({success: true}));
+	} catch(err) {
+		res.status(404).json({success: false, error: err.message});
+	}
 };
