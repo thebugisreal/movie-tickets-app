0189e10726e1080918fdb958ab6f6c06548909b9
thebugisreal <huyhung.dev@gmail.com>
2018-11-03 22:17:25 +0700

    create page not found

--- client/src/components/NotFound.js
+++ client/src/components/NotFound.js
@@ -1,11 +1,9 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class NotFound extends Component {
-  render() {
-    return (
-      <div>Here is NotFound component</div>
-    )
-  }
+export default () => {
+  return(
+    <div className="notFound w-100 h-100 py-5 d-flex position-relative justify-content-center align-items-center">
+      <img src="/static/notfound.png" alt="404 Not Found!" />
+    </div>
+  )
 }
-
-export default NotFound;
